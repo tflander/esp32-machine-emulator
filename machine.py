d3c4aef9de797e3b6526e32648455157a8635db5
Todd Flanders <toddflanders@Todds-MacBook-Pro.local>
2019-06-22 22:48:03 -0400

    initial commit

---
+++ machine.py
@@ -0,0 +1,54 @@
+"""
+The purpose of this module is to emulate the machine module of the ESP32 chip.  The primary reason to 
+emulate the chip is for test-driving code (TDD).
+
+Firmware version: esp32-20190610-v1.11-37-g62f004ba4
+"""
+import time
+EMULATION_MODE = True
+
+__version__ = '0.0.0'
+__author__ = 'Todd Flanders https://github.com/tflander/Esp32IotKata'
+__license__ = "Apache License 2.0. https://www.apache.org/licenses/LICENSE-2.0"
+
+expectedPulseTimeForTesting = 0
+expectedPulseTimeErrorForTesting = None
+
+def resetExpectationsForTesting():
+    global expectedPulseTimeForTesting, expectedPulseTimeErrorForTesting
+    expectedPulseTimeForTesting = 0
+    expectedPulseTimeErrorForTesting = None
+
+def time_pulse_us(pin, pulse_level, timeout_us):
+    global expectedPulseTimeErrorForTesting
+    if expectedPulseTimeErrorForTesting is not None:
+        raise expectedPulseTimeErrorForTesting
+    return expectedPulseTimeForTesting
+
+def sleep_us(delayUs):
+    pass
+
+time.sleep_us = sleep_us
+
+class Pin:
+    IN = "in"
+    OUT = "out"
+    pinForTesting = None
+    currentStateForTesting = None
+
+    def __init__(self, pin, mode=OUT, pull=None):
+        self.pinForTesting = pin
+
+    def on(self):
+        self.currentStateForTesting = 1
+
+    def off(self):
+        self.currentStateForTesting = 0
+
+    def value(self, newValue=None):
+        if newValue == 0:
+            self.off()
+        elif newValue == 1:
+            self.on()
+        return self.currentStateForTesting
+
---
+++ test_machine.py
@@ -0,0 +1,46 @@
+import machine
+import pytest
+
+@pytest.fixture()
+def resetMachine():
+    machine.resetExpectationsForTesting()
+    print("machine reset")
+
+class TestPinOut:
+    
+    def test_pinOnSetsValueHigh(self, resetMachine):
+        ledPin = machine.Pin(1, machine.Pin.OUT)
+        ledPin.on()
+        assert ledPin.value() == 1
+
+    def test_pinOffSetsValueLow(self, resetMachine):
+        ledPin = machine.Pin(1, machine.Pin.OUT)
+        ledPin.off()
+        assert ledPin.value() == 0
+
+    def test_pinValueHighSetsValueHigh(self, resetMachine):
+        ledPin = machine.Pin(1, machine.Pin.OUT)
+        ledPin.value(1)
+        assert ledPin.value() == 1
+
+    def test_pinValueLowSetsValueLow(self, resetMachine):
+        ledPin = machine.Pin(1, machine.Pin.OUT)
+        ledPin.value(0)
+        assert ledPin.value() == 0
+
+## TODO: Pin in tests
+
+class TestPulse:
+    
+    def test_returnsExpectedPulse(self, resetMachine):
+        machine.expectedPulseTimeForTesting = 123
+        assert machine.time_pulse_us(pin=1, pulse_level=1, timeout_us=1000) == 123
+
+    def test_returnsExpectedPulseTimeError(self, resetMachine):
+        machine.expectedPulseTimeErrorForTesting = OSError('error for testing')
+
+        try:
+            machine.time_pulse_us(pin=1, pulse_level=1, timeout_us=1000)
+            raise "Expected error"
+        except OSError as ex:
+            assert(ex.args[0] == 'error for testing')
